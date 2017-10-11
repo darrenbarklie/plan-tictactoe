e93ea428e30916ebd9c5df3512fd0d9105daa4ce
Darren Barklie <darrenbarklie@gmail.com>
2017-10-11 21:26:36 +0100

    Setup Sass, setup Board

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,11 +1,12 @@
-
 <div class="page page-home">
 
+  <!--Section : Welcome -->
   <section class="welcome">
     <h3>Welcome</h3>
     <p>Welcome to the game.  Follow the rules below.</p>
   </section><!--welcome-->
 
+  <!--Section : Rules -->
   <section class="rules">
     <h3>Rules</h3>
     <p>It's a battle of the bravest for the title of tic-tac-toe champion!</p>
@@ -16,6 +17,7 @@
     </ol>
   </section><!--rules-->
 
+  <!-- Section : Collect Names -->
   <section class="collect-names">
     <h3>Insert Names</h3>
     <p>Insert player names below:</p>
@@ -34,6 +36,5 @@
       <button type="submit" name="submit" class="btn btn-primary">Play</button>
     </div>
   </section><!--collect-names-->
-
 
 </div><!--page-home-->
---
+++ application/views/pages/play.php
@@ -0,0 +1,21 @@
+<div class="page page-play">
+
+  <!-- Section : UI -->
+  <header>
+    <!-- TODO : Add scores -->
+  </header>
+
+  <!-- Section : Board -->
+  <section class="board">
+    <div class="sq sq-1"></div>
+    <div class="sq sq-2"></div>
+    <div class="sq sq-3"></div>
+    <div class="sq sq-4"></div>
+    <div class="sq sq-5"></div>
+    <div class="sq sq-6"></div>
+    <div class="sq sq-7"></div>
+    <div class="sq sq-8"></div>
+    <div class="sq sq-9"></div>
+  </section><!--board-->
+
+</div><!--page-play-->
