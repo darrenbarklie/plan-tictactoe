019db88aeb3255a89d531b6527b2bdbb7f54d25b
Darren Barklie <darrenbarklie@gmail.com>
2017-10-11 19:35:33 +0100

    Basic templating and routing

---
+++ application/controllers/Pages.php
@@ -0,0 +1,16 @@
+<?php
+  class Pages extends CI_Controller{
+    // Default to homepage
+    public function view($page = 'home'){
+      // Check if view exists
+      if(!file_exists(APPPATH.'views/pages/'.$page.'.php')){
+        show_404();
+      }
+      // Pass variables to view
+      $data['title'] = ucfirst($page);
+      // Load page elements
+      $this->load->view('templates/header');
+      $this->load->view('pages/'.$page, $data);
+      $this->load->view('templates/footer');
+    }
+  }
