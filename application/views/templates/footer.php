9f6a478e5f231b8105eb806d0664debf4bed7c93
Darren Barklie <darrenbarklie@gmail.com>
2017-10-11 20:42:33 +0100

    Navigation and template setup

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,10 +1,12 @@
 
+    </div><!--container-->
+
+    <footer class="fixed-bottom bg-light">
+      <span class="copyright">&copy; 2017 Darren Barklie</span>
+    </footer>
 
     <script type="text/javascript" src="<?php echo base_url("assets/js/jquery-slim.min.js"); ?>"></script>
-
     <script type="text/javascript" src="<?php echo base_url("assets/js/popper.min.js"); ?>"></script>
-
     <script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.min.js"); ?>"></script>
-
   </body>
 </html>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -4,5 +4,29 @@
     <meta charset="utf-8">
     <title>TicTacToe</title>
     <link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.min.css"); ?>" />
+    <link rel="stylesheet" href="<?php echo base_url("assets/css/stylesheet.css"); ?>" />
   </head>
   <body>
+
+    <!-- Element : NAVBAR -->
+    <nav class="navbar navbar-expand-sm fixed-top navbar-light bg-light">
+      <div class="container">
+
+        <a class="navbar-brand" href="<?php echo base_url(); ?>">TicTacToe</a>
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavPrimary" aria-controls="navbarNavPrimary" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNavPrimary">
+          <div class="navbar-nav">
+            <a class="nav-item nav-link active" href="<?php echo base_url(); ?>">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-item nav-link" href="<?php echo base_url(); ?>about">About</a>
+          </div>
+        </div>
+      </div><!--container-->
+    </nav>
+    
+
+    <!-- Element : CONTENT CONTAINER -->
+    <div class="container content-container">
