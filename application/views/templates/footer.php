0d2d94411cc4da3ecb2f2ef85300faad1b410fda
Darren Barklie <darrenbarklie@gmail.com>
2017-10-13 20:11:45 +0100

    Media queries helper partial

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,11 +1,12 @@
 
-    </div><!--container-->
-
-    <footer class="fixed-bottom bg-light">
-      <div class="container">
-        <span class="copyright">&copy; 2017 Darren Barklie</span>
       </div><!--container-->
-    </footer>
+
+      <footer class="fixed-bottom bg-light">
+        <div class="container">
+          <span class="copyright">&copy; 2017 Darren Barklie</span>
+        </div><!--container-->
+      </footer>
+    </div><!--app-->
 
     <script type="text/javascript" src="<?php echo base_url("assets/js/jquery-slim.min.js"); ?>"></script>
     <script type="text/javascript" src="<?php echo base_url("assets/js/popper.min.js"); ?>"></script>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -7,26 +7,26 @@
     <link rel="stylesheet" href="<?php echo base_url("assets/css/stylesheet.css"); ?>" />
   </head>
   <body>
+    <div class="app">
+      <!-- Element : NAVBAR -->
+      <nav class="navbar navbar-expand-sm fixed-top navbar-light bg-light">
+        <div class="container">
 
-    <!-- Element : NAVBAR -->
-    <nav class="navbar navbar-expand-sm fixed-top navbar-light bg-light">
-      <div class="container">
+          <a class="navbar-brand" href="<?php echo base_url(); ?>">TicTacToe</a>
 
-        <a class="navbar-brand" href="<?php echo base_url(); ?>">TicTacToe</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavPrimary" aria-controls="navbarNavPrimary" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
 
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavPrimary" aria-controls="navbarNavPrimary" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarNavPrimary">
-          <div class="navbar-nav">
-            <a class="nav-item nav-link active" href="<?php echo base_url(); ?>play">Play <span class="sr-only">(current)</span></a>
-            <a class="nav-item nav-link" href="<?php echo base_url(); ?>about">About</a>
+          <div class="collapse navbar-collapse" id="navbarNavPrimary">
+            <div class="navbar-nav">
+              <a class="nav-item nav-link active" href="<?php echo base_url(); ?>play">Play <span class="sr-only">(current)</span></a>
+              <a class="nav-item nav-link" href="<?php echo base_url(); ?>about">About</a>
+            </div>
           </div>
-        </div>
-      </div><!--container-->
-    </nav>
+        </div><!--container-->
+      </nav>
 
 
-    <!-- Element : CONTENT CONTAINER -->
-    <div class="container content-container">
+      <!-- Element : CONTENT CONTAINER -->
+      <div class="container content-container">
