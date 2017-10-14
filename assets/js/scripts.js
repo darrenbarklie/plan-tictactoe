22bef08067b2b0f8c3d133271f553719ba481e94
Darren Barklie <darrenbarklie@gmail.com>
2017-10-14 01:57:40 +0100

    Set state on select

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -19,7 +19,6 @@ $( document ).ready(function() {
   // Match grid height to grid width
   let applyBoardHeight = debounce(function(){
     let boardWidth = $('#board').outerWidth();
-    console.log(boardWidth);
     $('#board').outerHeight(boardWidth);
   }, 100);
 
@@ -27,5 +26,140 @@ $( document ).ready(function() {
   $(window).on("resize", function () {
     applyBoardHeight();
   }).resize();
+
+
+
+
+
+
+
+
+
+
+
+  // GAME LOGIC
+
+
+
+
+
+
+
+
+
+
+
+  let board = $('#board');
+  let square = $('#board .sq span');
+  let player = 1;
+
+  // selectSquare - PROGRESS
+  $(square).click(function(){
+    square = $(this);
+    let state = getSqState(square);
+
+    // Debug
+    console.log(state);
+  });
+
+  // getSqState - WORKS
+  function getSqState(square){
+    if(square.hasClass('selected-x') || square.hasClass('selected-o')) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+  // assignPlayer
+  function assignPlayer(player){
+    if(player == 1){
+      return 'selected-x';
+    } else {
+      return 'selected-o';
+    }
+  }
+
+  // swapPlayer
+  function swapPlayer(player){
+    if(player == 1){
+      return player = 2;
+    } else {
+      return player = 1;
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //selectSquare
+    // $(square).click(function(){
+    //   if($(this).hasClass('selected')){
+    //     $(this).removeClass('selected');
+    //   } else {
+    //     $(this).addClass('selected');
+    //   }
+    // });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 });
