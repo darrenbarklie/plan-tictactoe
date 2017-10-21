b68a7f9fbf3e77eb2711411c55272ab2e2ced4ea
Darren Barklie <darrenbarklie@gmail.com>
2017-10-21 17:12:24 +0100

    Connect to database

--- application/controllers/Games.php
+++ application/controllers/Games.php
@@ -3,6 +3,12 @@
     public function index(){
       $data['title'] = 'Previous 5 Games';
 
+      $data['games'] = $this->game_model->get_games();
+
+      print_r($data['games']);
+
+
+
       $this->load->view('templates/header');
       $this->load->view('games/index', $data);
       $this->load->view('templates/footer');
--- application/models/Game_model.php
+++ application/models/Game_model.php
@@ -4,7 +4,13 @@
       $this->load->database();
     }
 
-    public function get_games(){
+    public function get_games($id = FALSE){
+      if($id === FALSE){
+        $query = $this->db->get('games');
+        return $query->result_array();
+      }
 
+      $query = $this->db->get_where('games', array('id' => $id));
+      return $query->row_array();
     }
   }
